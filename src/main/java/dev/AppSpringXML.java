b80f4d5827f8a94add3baf5a062c1ec3d4bd8821
Gaëlle BROSSAS <gaellebrossas@orange.fr>
2020-07-10 14:27:08 +0200

    TP1 : complet

--- src/main/java/dev/AppSpringXML.java
+++ src/main/java/dev/AppSpringXML.java
@@ -4,12 +4,20 @@ import java.util.Scanner;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import dev.ihm.Menu;
+
 public class AppSpringXML {
 
 	public static void main(String[] args) {
 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-config-memoire.xml");
-		Scanner scanner = context.getBean(Scanner.class);
+		// Création du contexte Spring
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-config-fichier.xml");
+		// récupération du bean Menu
+		Menu menu = context.getBean(Menu.class);
+		menu.afficher();
+		// fermeture du Scanner
+		context.getBean(Scanner.class).close();
+		// fermeture du contexte Spring
 		context.close();
 
 	}
