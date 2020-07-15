6639983b37e7e29a356f197bb34cfec5183e00f3
Gaëlle BROSSAS <gaellebrossas@orange.fr>
2020-07-15 15:59:37 +0200

    TP 3.3

--- src/test/java/dev/service/PlatServiceVersion2IntegrationTest.java
+++ src/test/java/dev/service/PlatServiceVersion2IntegrationTest.java
@@ -1,6 +1,9 @@
 package dev.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import dev.config.AppConfig;
+import dev.entite.Plat;
 import dev.exception.PlatException;
 
 @SpringJUnitConfig(AppConfig.class)
@@ -20,6 +24,9 @@ public class PlatServiceVersion2IntegrationTest {
 	@Test
 	void ajouterPlatValide() {
 		service2.ajouterPlat("dessert", 1500);
+		List<Plat> resultat = service2.listerPlats();
+		assertThat(resultat).extracting(Plat::getNom).containsExactly("dessert");
+		assertThat(resultat).extracting(Plat::getPrixEnCentimesEuros).containsExactly(1500);
 	}
 	
 	@Test
